0f7824777890de53c928687e7934f6b9fa06f44d
grin-69 <anikettahabildar69@gmail.com>
2025-04-23 23:20:23 +0530

    Update hindi.py

--- data/hindi.py
+++ data/hindi.py
@@ -1,3 +1,3 @@
 HINDI_PROFANITY = [
-  "bc", "mc", "lawra", "motherchod", "betichod", "lund"
+  "bc", "mc", "lawra", "motherchod", "betichod", "lund","bahan ka loda","bkl","maa ka chut","mkc","gand"
 ]
